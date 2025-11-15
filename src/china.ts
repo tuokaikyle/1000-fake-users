9410e200bbbee36c59bb5c13cee66adb48c25221
tuokaikyle <kaituo@outlook.com>
2025-11-15 15:17:50 +1000

    Chinese username

--- src/china.ts
+++ src/china.ts
@@ -1,26 +1,26 @@
 import { fakerZH_CN as faker } from '@faker-js/faker'
 import { bloodTypeWeighted, chinaRelatedCountries, MBTI_TYPES, relationshipStatusWeighted, type User } from './types'
-import { getNormalDistributionData, getZodiacSign } from './utils'
+import { getNormalDistributionData, getZodiacSign, commonFamilyNames, addPrefixToDuplicateEmails } from './utils'
 import fs from 'fs'
 import Papa from 'papaparse'
 
 function createRandomUser(): User {
 	const sex = faker.person.sexType()
-	const username = faker.person.lastName()
+	const username = faker.helpers.arrayElement(commonFamilyNames) + String(Math.floor(Math.random() * 10000))
 	const password = faker.internet.password()
 	const dob = faker.date.birthdate({ mode: 'year', min: 1980, max: 2005 }).toISOString().split('T')[0] as string
-	const email = faker.internet.email() // unrelated to names, can have duplicates
+	const email = faker.internet.email() // unrelated to names, can have duplicates, apply addPrefixToDuplicateEmails below
 	// faker.helpers.uniqueArray(faker.internet.email, 1000); // no duplicates
 
 	const fromLocation = faker.helpers.weightedArrayElement([
 		{ weight: 90, value: faker.location.state() },
-		{ weight: 5, value: faker.helpers.arrayElement(['香港特别行政区', '澳门特别行政区', '台湾地区']) },
+		{ weight: 5, value: faker.helpers.arrayElement(['香港特别行政区', '澳门特别行政区', '台湾省']) },
 		{ weight: 5, value: faker.helpers.weightedArrayElement(chinaRelatedCountries) },
 	])
 
 	const currentLocation = faker.helpers.weightedArrayElement([
 		{ weight: 70, value: faker.location.state() },
-		{ weight: 10, value: faker.helpers.arrayElement(['香港特别行政区', '澳门特别行政区', '台湾地区']) },
+		{ weight: 10, value: faker.helpers.arrayElement(['香港特别行政区', '澳门特别行政区', '台湾省']) },
 		{ weight: 20, value: faker.helpers.weightedArrayElement(chinaRelatedCountries) },
 	])
 
@@ -56,6 +56,8 @@ function createRandomUser(): User {
 	}
 }
 
-const fakeUserData = faker.helpers.multiple(createRandomUser, { count: 1000 })
+let fakeUserData = faker.helpers.multiple(createRandomUser, { count: 1000 })
+fakeUserData = addPrefixToDuplicateEmails(fakeUserData)
+
 const csv = Papa.unparse(fakeUserData)
 fs.writeFileSync('1000-fake-users-china.csv', csv)
